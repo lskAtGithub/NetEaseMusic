191795c753a4eac6ce6565fa6817a79ee32fcab0
sk <949919743@qq.com>
2017-12-09 04:50:45 +0800

    歌词滚动效果

--- song.js
+++ song.js
@@ -6,35 +6,74 @@ $(function (){
   $.get('./songs.json').then(function (response){
     let songs = response
     let song = songs.filter(s=>s.id == id)[0]
-    let {url} = song
-    let audio = document.createElement('audio')
-      audio.src = url
-      audio.oncanplay = function (){
-        audio.play()
-        $('.disc-container').addClass('playing')
-      }
+    let {url,songname,lyric} = song
 
-      $('.icon-pause').on('click',function (){
-        audio.pause()
-        $('.disc-container').removeClass('playing')
-      })
-      $('.icon-play').on('click',function (){
-        audio.play()
-        $('.disc-container').addClass('playing')
-      })
+    initText(songname,lyric)
+    initPlayer.call(undefined,url)
     })
   })
 
-  /* 通过get获取歌词，拼接并生成获取的歌词 */
-  $.get('/lyric.json').then(function (object){
-    let {lyric} = object
+
+
+  function initText(name,lyric){
+    $('#singer').text(name)
+    parseLyric(lyric)
+
+  }
+  function initPlayer(url){
+    let audio = document.createElement('audio')
+    audio.src = url
+    audio.oncanplay = function (){
+      audio.play()
+      $('.disc-container').addClass('playing')
+    }
+
+    $('.icon-pause').on('click',function (){
+      audio.pause()
+      $('.disc-container').removeClass('playing')
+    })
+    $('.icon-play').on('click',function (){
+      audio.play()
+      $('.disc-container').addClass('playing')
+    })
+
+    setInterval(function (){
+      let seconds = audio.currentTime
+      let munites = ~~(seconds / 60)
+      let left = seconds - munites * 60
+      let time = `${pad(munites)}:${pad(left)}`
+      let $lines = $('.lines> p')
+      let $whichLine
+      for(let i = 0; i < $lines.length; i++){
+        if($lines.eq(i).attr('data-time') <= time && $lines.eq(i+1).attr('data-time') > time){
+         $whichLine = $lines.eq(i)
+          break
+        }
+      }
+      if($whichLine){
+        $whichLine.addClass('active').prev().removeClass('active')
+        let top = $whichLine.offset().top
+        let linesTop = $('.lines').offset().top
+        let delta = top - linesTop - $('.lyric').height() / 3
+        $('.lines').css('transform',`translateY(-${delta}px)`)
+        
+      }
+    },300)
+  }
+
+
+  function pad(num){
+    return num>=10 ? num + '' : '0' + num
+  }
+
+  function parseLyric(lyric){
     let arr = lyric.split('\n')
     let regex = /^\[(.+)\](.*)/
     arr = arr.map(function (str){
       let matches = str.match(regex)
       if(matches){
         return {time: matches[1],words: matches[2]}
-      } 
+      }
     })
     /* 生成歌词 */
     var $lyric = $('.lyric .lines')
@@ -44,6 +83,4 @@ $(function (){
       $p.attr('data-time',object.time).text(object.words)
       $p.appendTo($lyric)
     })
-  })
-
-  
+  }
