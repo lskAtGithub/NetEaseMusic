14540c3a39f818e4518d4e5425c2db3ae2cbb6af
sk <949919743@qq.com>
2017-12-09 22:41:00 +0800

    search（未完成）

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $(function (){
-
+  /* 推荐音乐 */
   $.get('./songs.json').then(function (response){
     let items = response;
     items.forEach((i) => {
@@ -29,4 +29,32 @@ $(function (){
     $('.tabs-content').children().eq(index)
      .addClass('active').siblings().removeClass('active')
   })
+
+  /* 热歌榜 */
+  $.get('./songs.json').then(function (response){
+    let items = response;
+    items.forEach((i) => {
+      let $li = $(`
+        <li>      
+        <a href="./song.html?id=${i.id}">
+          <h3>${i.songname}</h3>
+          <p><svg class="sq">
+            <use xlink:href="#icon-sq"></use>
+          </svg>${i.singer}</p>
+          <svg class="play">
+            <use xlink:href="#icon-play1"></use>
+          </svg>
+        </a>
+      </li>
+      `)
+      $('#hot').append($li);
+    });
+    $('#page2-loading').remove();
+  })
+
+  var today=new Date();
+  
+  var time=today.getFullYear()+"年"+today.getMonth()+"月"+today.getDate()+"日"
+  $('#time').text('更新日期：' + time)
+
 })
